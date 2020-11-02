980f37b18f85bc1a1499d448ccd4a8eeeefe9fa6
Sean Perkins <seanperkins@Seans-MBP.hsd1.in.comcast.net>
2020-11-02 11:11:42 -0500

    added iframe resume

--- porfolio.js
+++ porfolio.js
@@ -2,22 +2,33 @@ $('#my-linkH').click(function(e) {
     $('#collapseOne').collapse('hide');
     $('#collapseTwo').collapse('hide');
     $('#collapseThree').collapse('hide');
+    $('#collapseFour').collapse('hide');
 })
 
 $('#my-link1').click(function(e) {
     $('#collapseOne').collapse('show');
     $('#collapseTwo').collapse('hide');
     $('#collapseThree').collapse('hide');
+    $('#collapseFour').collapse('hide');
 })
 
 $('#my-link2').click(function(e) {
     $('#collapseTwo').collapse('show');
     $('#collapseOne').collapse('hide');
     $('#collapseThree').collapse('hide');
+    $('#collapseFour').collapse('hide');
 })
 
 $('#my-link3').click(function(e) {
     $('#collapseThree').collapse('show');
+    $('#collapseOne').collapse('hide');
+    $('#collapseTwo').collapse('hide');
+    $('#collapseFour').collapse('hide');
+})
+
+$('#my-link4').click(function(e) {
+    $('#collapseFour').collapse('show');
+    $('#collapseThree').collapse('hide');
     $('#collapseOne').collapse('hide');
     $('#collapseTwo').collapse('hide');
 })
@@ -26,4 +37,5 @@ $('#my-linkC').click(function(e) {
     $('#collapseOne').collapse('hide');
     $('#collapseTwo').collapse('hide');
     $('#collapseThree').collapse('hide');
+    $('#collapseFour').collapse('hide');
 })
